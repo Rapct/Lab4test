42145cead21d7e3ac425b998955c2577e7d69b08
Rapct <linmeng@kean.edu>
2024-03-04 14:47:01 +0800

    first commit

---
+++ linmeng/Controllers/UsersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using linmeng.Models;
+namespace linmeng.Controllers
+{
+    public class UsersController : Controller
+    {
+        UsersContext uc = new();
+        public IActionResult linView()
+        {
+            return View(uc.LinmengTables.ToList());
+        }
+    }
+}
---
+++ linmeng/Models/LinmengTable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace linmeng.Models;
+
+public partial class LinmengTable
+{
+    public int UserId { get; set; }
+
+    public string? UserName { get; set; }
+
+    public string? UserEmail { get; set; }
+}
